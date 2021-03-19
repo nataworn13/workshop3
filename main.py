2c1102a6c1c8988f9166bbe27f0cc5cf7a0c1f93
nataworn13 <nut_nataworn@hotmail.com>
2021-03-19 23:01:55 +0700

    no message

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def index():
     return JSONResponse(content={"message": "Hello,  World"}, status_code=200)
 
 
-if name == "main":
-    uvicorn.run("main:app", host="127.0.0.1", port=3001, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=3000, reload=True)
